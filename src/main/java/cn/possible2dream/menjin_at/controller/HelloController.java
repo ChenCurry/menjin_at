c1b0a74388eaf81b8c527baf41b130878780ae39
xiaohong <1714616511@qq.com>
2020-05-14 23:32:26 +0800

    一个注解

--- src/main/java/cn/possible2dream/menjin_at/controller/HelloController.java
+++ src/main/java/cn/possible2dream/menjin_at/controller/HelloController.java
@@ -2,7 +2,6 @@ package cn.possible2dream.menjin_at.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -11,7 +10,7 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
-    @ResponseBody
+    //@ResponseBody
     @RequestMapping("/hello")//这个接口的名字就叫做http://localhost:8080/hello
     //// 它就单纯的成了一个接口 不用再去管web.xml  前端  等等一系列东西了 它就和vue完全前后端分离了
     public String hello(){
